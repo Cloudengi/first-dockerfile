7384a8e2320d5722a41f51777e7d3cd9cdcb7631
Cloudengi <rs17690@gmail.com>
2025-02-16 16:46:19 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+python
+
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route("/")
+def hello():
+    return "Hello, World! This is running in Docker!"
+
+if _name_ == "_main_":
+   app.run(host="0.0.0.0", port=5000)
